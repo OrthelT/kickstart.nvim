c2350be8859d50224e8c0302a0034d537629f36c
Orthel Toralen <orthel.toralen@gmail.com>
2025-06-11 01:21:01 -0400

    Merged upstream with updated init.lua

--- init.lua
+++ init.lua
@@ -774,7 +774,6 @@ require('lazy').setup({
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
       },
-
       sources = {
         default = { 'lsp', 'path', 'snippets' },
       },
